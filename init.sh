5cef858f004e66896b1719bd5678172750f58086
snyh <snyh@snyh.org>
2020-07-23 22:52:14 +0800

    init

---
+++ init.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+uu=$SUDO_USER
+
+export XDG_RUNTIME_DIR=/run/user/$SUDO_UID
+export USER=$uu
+export LONGNAME=$uu
+export HOME=/home/$uu
+mount --make-private --bind root.x86_64/ root.x86_64/
+cleanup()
+{
+    umount -l ./root.x86_64/
+    exit
+}
+
+trap cleanup EXIT
+
+./arch-chroot -u $SUDO_USER:$SUDO_USER root.x86_64
